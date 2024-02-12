e4c002129a54180f3eade7c2acefe46d6d3f5e0e
Koeboldt <142840132+Koeboldt@users.noreply.github.com>
2024-02-12 17:42:43 -0600

    Set up the file structure as well as set up the schema

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        _id: true,
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }],
+        friends:[{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+      }
+);
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+const User = model('user', userSchema);
+
+module.exports = User;
+/*Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query. */
